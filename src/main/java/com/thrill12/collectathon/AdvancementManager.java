c3386b06f25425de87389eaaa44f62d35fc906ea
AndreiPC <andreipatea@gmail.com>
2026-05-28 20:21:39 +0100

    sets

--- src/main/java/com/thrill12/collectathon/AdvancementManager.java
+++ src/main/java/com/thrill12/collectathon/AdvancementManager.java
@@ -3,6 +3,8 @@ package com.thrill12.collectathon;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -17,6 +19,7 @@ public class AdvancementManager {
     @SubscribeEvent
     public static void onServerStarting(ServerStartingEvent event) {
         generateDatapack(event.getServer());
+        event.getServer().execute(() -> event.getServer().getPlayerList().reloadResources());
     }
 
     private static void generateDatapack(MinecraftServer server) {
@@ -33,11 +36,33 @@ public class AdvancementManager {
             JsonObject root = buildRootAdvancement();
             Files.writeString(datapackPath.resolve("root.json"), gson.toJson(root));
 
-            // Write one advancement per card
+            Map<String, Path> setPaths = new HashMap<>();
+
             for (CardData card : CardManager.getCards()) {
-                JsonObject advancement = buildCardAdvancement(card);
-                Files.writeString(datapackPath.resolve(card.id() + ".json"),
-                        gson.toJson(advancement));
+
+                String rawSet = card.set(); // "&4Swamp"
+                String setId = toSafeId(rawSet); // "swamp"
+
+                Path setPath = setPaths.get(setId);
+
+                // 1. create set folder once
+                if (setPath == null) {
+                    Collectathon.LOGGER.info("Creating advancement set: {}", setId);
+                    setPath = datapackPath.resolve(setId);
+                    Files.createDirectories(setPath);
+
+                    JsonObject setAdv = buildSetAdvancement(rawSet, setId);
+                    Files.writeString(setPath.resolve("set.json"), gson.toJson(setAdv));
+
+                    setPaths.put(setId, setPath);
+                }
+
+                // 2. write card under set
+                JsonObject advancement = buildCardAdvancement(card, setId);
+
+                Collectathon.LOGGER.info("Creating advancement for card: {}", card.id());
+
+                Files.writeString(setPath.resolve(card.id() + ".json"), gson.toJson(advancement));
             }
 
             Collectathon.LOGGER.info("Generated advancements for {} cards",
@@ -46,6 +71,36 @@ public class AdvancementManager {
         } catch (IOException e) {
             Collectathon.LOGGER.error("Failed to generate card advancements", e);
         }
+    }
+
+    private static JsonObject buildSetAdvancement(String setDisplayName, String setId) {
+        JsonObject obj = new JsonObject();
+
+        obj.addProperty("parent", "collectathon:cards/root");
+
+        JsonObject display = new JsonObject();
+        JsonObject icon = new JsonObject();
+
+        icon.addProperty("id", "minecraft:book");
+
+        display.add("icon", icon);
+        display.addProperty("title", setDisplayName);
+        display.addProperty("description", "Card collection: " + setId);
+
+        display.addProperty("frame", "challenge");
+        display.addProperty("show_toast", false);
+        display.addProperty("announce_to_chat", false);
+
+        obj.add("display", display);
+
+        JsonObject criteria = new JsonObject();
+        JsonObject c = new JsonObject();
+        c.addProperty("trigger", "minecraft:tick");
+        criteria.add("unlock", c);
+
+        obj.add("criteria", criteria);
+
+        return obj;
     }
 
     private static void writePackMeta(Path datapackRoot) throws IOException {
@@ -64,6 +119,7 @@ public class AdvancementManager {
         JsonObject display = new JsonObject();
         JsonObject icon = new JsonObject();
         icon.addProperty("id", "collectathon:card");
+
         display.add("icon", icon);
         display.addProperty("title", "Card Collector");
         display.addProperty("description", "Start collecting cards!");
@@ -82,22 +138,23 @@ public class AdvancementManager {
         return obj;
     }
 
-    private static JsonObject buildCardAdvancement(CardData card) {
+    private static JsonObject buildCardAdvancement(CardData card, String set) {
         JsonObject obj = new JsonObject();
 
         // Parent
-        obj.addProperty("parent", "collectathon:cards/root");
+        obj.addProperty("parent", "collectathon:cards/" + set + "/set");
 
         // Display
         JsonObject display = new JsonObject();
         JsonObject icon = new JsonObject();
         icon.addProperty("id", "collectathon:card");
+
         display.add("icon", icon);
         display.addProperty("title",
                 card.shiny() ? "Shiny " + card.displayName() : card.displayName());
         display.addProperty("description",
                 "Obtained " + (card.shiny() ? "Shiny " + card.displayName() : card.displayName()));
-        display.addProperty("frame", card.shiny() ? "challenge" : "task");
+        display.addProperty("frame", "task");
         display.addProperty("show_toast", true);
         display.addProperty("announce_to_chat", true);
         obj.add("display", display);
@@ -113,5 +170,10 @@ public class AdvancementManager {
         obj.add("criteria", criteria);
 
         return obj;
+    }
+
+    private static String toSafeId(String input) {
+        return input.replaceAll("&[0-9a-fk-or]", "") // remove color codes
+                .toLowerCase().replace(" ", "_").replaceAll("[^a-z0-9_/]", "");
     }
 }
